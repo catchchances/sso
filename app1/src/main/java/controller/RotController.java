fbfee9d0902083b5556d672cc835a082db4a7a34
long.zhang <long.zhang@riking.net>
2018-07-10 15:20:46 +0800

    新建两个SpringMVC项目

---
+++ app1/src/main/java/controller/RotController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class RotController {
+
+	@RequestMapping(value = "/msg", method = RequestMethod.GET)
+	public String root() {
+		return "You did it";
+	}
+
+}
